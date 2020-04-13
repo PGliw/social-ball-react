ff88972be865b2decf362afa0245809edd5154ec
Piotr Chorościn <titkot96@gmail.com>
2020-04-13 10:52:34 +0200

    Updates

---
+++ precache-manifest.bf4a360979d9255cf9866820d9173c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab5419694f5af6e6aed279ff953b8d0",
+    "url": "/social-ball-react/index.html"
+  },
+  {
+    "revision": "d0e4f9b8b31bc0b5468e",
+    "url": "/social-ball-react/static/css/main.47e42611.chunk.css"
+  },
+  {
+    "revision": "88a1278633021b01e148",
+    "url": "/social-ball-react/static/js/2.c11c7243.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-ball-react/static/js/2.c11c7243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e4f9b8b31bc0b5468e",
+    "url": "/social-ball-react/static/js/main.72523800.chunk.js"
+  },
+  {
+    "revision": "ad5721971cac691103dc",
+    "url": "/social-ball-react/static/js/runtime-main.a145b8d3.js"
+  }
+]);
