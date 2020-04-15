4a0d9012c99ceeb9329776079ed5d98f0d2cc499
Piotr Chorościn <titkot96@gmail.com>
2020-04-15 16:09:29 +0200

    Change drawer to persistent

--- src/NavDrawer.js
+++ src/NavDrawer.js
@@ -1,47 +1,111 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Appbar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import {ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import SportsSoccerIcon from '@material-ui/icons/SportsSoccer';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import IconButton from '@material-ui/core/IconButton';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-        root: {
-            display: 'flex',
-        },
-        appBar: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth,
-            backgroundColor: "#32a852",
-        },
-        drawer: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
-        drawerPaper: {
-            width: drawerWidth,
-        },
-        toolbar: (theme.mixins.toolbar),
-        content: {
-            flexGrow: 1,
-            backgroundColor: theme.palette.background.default,
-            padding: theme.spacing(3),
-        },
-    }));
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        backgroundColor: "#478101",
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        display: "inline-block"
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    toolbar: (theme.mixins.toolbar),
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+}));
 
-const NavDrawer = ({ children }) => {
+const NavDrawer = ({children}) => {
     const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true)
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false)
+    };
 
     return (
         <div className={classes.root}>
-            <CssBaseline />
+            <CssBaseline/>
             <Appbar position="fixed" className={classes.appBar}>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    className={clsx(classes.menuButton, open && classes.hide)}
+                >
+                    <MenuIcon/>
+                </IconButton>
                 <Toolbar>
                     <Typography variant="h6" noWrap>
                         Social-ball
@@ -50,28 +114,42 @@ const NavDrawer = ({ children }) => {
             </Appbar>
             <Drawer
                 className={classes.drawer}
-                variant="permanent"
+                variant="persistent"
+                open={open}
                 classes={{
                     paper: classes.drawerPaper,
                 }}
                 anchor="left">
-                <div className={classes.toolbar} />
-                <Divider />
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+                    </IconButton>
+                </div>
+                <Divider/>
                 <List>
                     <ListItem>
-                        <ListItemText>Opcja nr 1</ListItemText>
+                        <ListItemIcon><SportsSoccerIcon/></ListItemIcon>
+                        <ListItemText>Mecze</ListItemText>
                     </ListItem>
                     <ListItem>
-                        <ListItemText>Opcja nr 2</ListItemText>
+                        <ListItemIcon><EqualizerIcon/></ListItemIcon>
+                        <ListItemText>Statystyki</ListItemText>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><AccountBoxIcon/></ListItemIcon>
+                        <ListItemText>Profil</ListItemText>
                     </ListItem>
                 </List>
             </Drawer>
-            <main className={classes.content}>
-                <div className={classes.toolbar} />
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
                 {children}
             </main>
         </div>
     );
-}
+};
 
 export default NavDrawer;
